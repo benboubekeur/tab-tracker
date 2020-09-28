dd2d2c032ea10e6262849c7daadb9d7e12920300
boumedyen <boumedyen.benboubekeur@gmail.com>
2020-09-28 12:19:55 +0100

    FEAT : Add show song button

---
+++ client/src/components/ShowSong.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Hello</h1>
+</template>
+
+<script>
+export default {
+  name: 'ShowSong'
+}
+</script>
+
+<style scoped>
+
+</style>
--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -1,47 +1,58 @@
 <template>
-    <v-layout column>
-        <v-flex xs6>
-            <panel title="Songs">
-                <v-btn
-                        slot="action"
-                        :to="{name: 'songs-create'}"
-                        class="cyan accent-2"
-                        light
-                        absolute
-                        right
-                        medium
-                        fab>
-                    <v-icon>add</v-icon>
-                </v-btn>
-                <div class="song" v-for="song in songs" :key="song.id">
-                    <v-layout >
-                        <v-flex xs6>
-                                <div class="song-title">
-                                    {{song.title}}
+    <panel title="Songs">
+        <v-btn
+                slot="action"
+                :to="{
+        name: 'songs-create'
+      }"
+                class="cyan accent-2"
+                light
+                medium
+                absolute
+                right
+                middle
+                fab>
+            <v-icon>add</v-icon>
+        </v-btn>
 
-                                </div>
-                                <div class="song-artiste">
-                                    {{song.artiste}}
+        <div
+                v-for="song in songs"
+                class="song"
+                :key="song.id">
 
-                                </div>
-                                <div class="song-genre">
-                                    {{song.genre}}
+            <v-layout>
+                <v-flex xs6>
+                    <div class="song-title">
+                        {{song.title}}
+                    </div>
+                    <div class="song-artist">
+                        {{song.artist}}
+                    </div>
+                    <div class="song-genre">
+                        {{song.genre}}
+                    </div>
 
-                                </div>
+                    <v-btn
+                            dark
+                            class="cyan"
+                            :to="{
+              name: 'song',
+              params: {
+                songId: song.id
+              }
+            }">
+                        View
+                    </v-btn>
+                </v-flex>
 
-                        </v-flex>
-                        <v-flex xs6>
-                            <img :src="song.albumImage"  class="album-image">
-                        </v-flex>
-                    </v-layout>
-
-                    {{song.album}}-
-                    {{song.artiste}}-
-                </div>
-            </panel>
-        </v-flex>
-    </v-layout>
+                <v-flex xs6>
+                    <img class="album-image" :src="song.albumImage" />
+                </v-flex>
+            </v-layout>
+        </div>
+    </panel>
 </template>
+
 <script>
 import Panel from '@/components/Panel'
 import SongsService from '@/services/SongsService'
@@ -62,25 +73,23 @@ export default {
   }
 }
 </script>
-
 <style scoped>
-    .song{
-padding: 20px;
+    .song {
+        padding: 20px;
         height: 330px;
         overflow: hidden;
     }
-.album-image{
-    width: 65%;
-    margin: 0 auto;
-}
-.song-title{
-    font-size: 30px;
-
-}
-.song-artiste{
-    font-size: 24px;
-}
-    .song-genre{
+    .song-title {
+        font-size: 30px;
+    }
+    .song-artist {
+        font-size: 24px;
+    }
+    .song-genre {
         font-size: 18px;
+    }
+    .album-image {
+        width: 70%;
+        margin: 0 auto;
     }
 </style>
